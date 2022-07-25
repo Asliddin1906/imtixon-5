f5cdbb37fc43b404377bd4f008f9e6ddc6a5c6e5
Asliddin1906 <umaraliyev04@gmail.com>
2022-07-25 20:45:23 +0500

    last-commit

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -13,12 +13,12 @@ const Head = () => {
     <div className="dropdown">
     <button className="dropbtn"><img src={bars} alt="" className="menu-btn" /><img src={close} alt="" className="close-btn"/> </button>
       <div className="dropdown-content nav" >
-        <a href="*" className="nav__item">ALL</a>
-        <a href="*" className="nav__item">Design</a>
-        <a href="*" className="nav__item">Theory</a>
-        <a href="*" className="nav__item">UX</a>
-        <a href="*" className="nav__item">UI</a>
-        <a href="*" className="nav__item"> Typography</a>
+        <a href="#" className="nav__item">ALL</a>
+        <a href="#" className="nav__item">Design</a>
+        <a href="#" className="nav__item">Theory</a>
+        <a href="#" className="nav__item">UX</a>
+        <a href="#" className="nav__item">UI</a>
+        <a href="#" className="nav__item"> Typography</a>
       </div>
     </div>
     </div>
